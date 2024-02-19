861b8bf32c85041a03b641beb477ca8352ad6b77
Anuj Kumar Chaudhary <117503109+akc2531@users.noreply.github.com>
2024-02-19 14:10:39 +0530

    Create Keys.java

---
+++ Keys.java
@@ -0,0 +1,19 @@
+package org.eamples.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SendKeys {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.findElement(By.id("email")).clear();
+        driver.findElement(By.id("email")).sendKeys("anujchaudhary2531@gmail.com");
+        driver.findElement(By.id("pass")).clear();
+        driver.findElement(By.id("pass")).sendKeys("kkk");
+        driver.findElement(By.id("pass")).sendKeys(Keys.ENTER);
+    }
+}
